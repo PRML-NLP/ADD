82211ed16f84bed302b1c837a4f4865df92ac8c3
SinGonKim <singon96@naver.com>
2025-02-14 17:13:50 +0900

    Fix run_task.sh

--- Scripts/run_task.sh
+++ Scripts/run_task.sh
@@ -4,4 +4,6 @@ python Self_Plan/Traj_Syn/run_task.py \
     --max_context_len 4096 \
     --task Hotpotqa \
     --task_path Self_Instruct/Meta_sample/Meta_Hotpotqa.json \
-    --save_path Self_Plan/Traj_Syn/output/hotpotqa_train_data.jsonl
+    --save_path Self_Plan/Traj_Syn/output/hotpotqa_train_data.jsonl\
+    --use_openai False \
+    --openai_key EMPTY
